d38f380d7489ccccae2f96be6c867cedaaebf402
Phillips <phillipsw1@wit.edu>
2019-05-11 01:32:00 -0400

    set production database address to env

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI: ''}
+    module.exports = {mongoURI: process.env.DATABASE_SERVER}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
 }
